63f76e8230a8088fe987d3eff7c92320652ce304
LoLka212 <91520018+LoLka212@users.noreply.github.com>
2022-11-18 20:34:19 +0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -16,7 +16,7 @@ async def hello(inter):
     await inter.response.send_message("World")
 
 @bot.slash_command()
-async def ping(inter,ctx, amount: int=None):
-    await inter.response.send_message("Pong!")
+async def Hey!(inter,ctx, amount: int=None):
+    await inter.response.send_message("Hi!")
 
 bot.run(os.getenv('TOKEN'))
