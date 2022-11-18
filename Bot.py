e17d87993db11a954351f6789ae65403611aa40e
LoLka212 <91520018+LoLka212@users.noreply.github.com>
2022-11-18 22:45:46 +0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -7,6 +7,17 @@ from disnake.ext import commands
 
 bot = commands.InteractionBot(intents=disnake.Intents.all())
 
+@bot.slash_command(description="Replies with the given text!")
+async def echo(
+    inter: disnake.ApplicationCommandInteraction,
+    text: str = commands.Param(description="Echo~"),
+) -> None:
+    await inter.response.send_message(text)
+
+@bot.slash_command(description="Responds with 'World'")
+async def hello(inter,ctx, amount: int=None):
+    await inter.response.send_message("World")
+
 @bot.slash_command()
 async def ping(inter,ctx, amount: int=None):
     await inter.response.send_message("Pong!")
